250f7a23632961b9ef58131276b7135faec704b9
DamianMatysko <44482004+DamianMatysko@users.noreply.github.com>
2020-04-30 20:17:28 +0200

    Initial commit

---
+++ src/main/java/sample/DateAndTime.java
@@ -0,0 +1,58 @@
+package sample;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DateAndTime {
+    List<User> list =  new ArrayList<User>();
+
+    public DateAndTime() {
+        list.add(new User("Roman","Simko","roman","heslo"));
+    }
+
+    @RequestMapping("/time")
+    public String getTime() {
+        return "11:29:15";
+    }
+
+    @RequestMapping("/primenumber/{number}")
+    public String checkPrimeNumber() {
+        return "true / false";
+    }
+
+    @RequestMapping("/time/hour")
+    public String getHour(){
+        return "11";
+    }
+
+    @RequestMapping("/hello")
+    public String getHello(){
+        return "Hello. How are you? ";
+    }
+
+    @RequestMapping("/hello/{name}")
+    public String getHelloWithName(@PathVariable String name){
+        return "Hello "+name+". How are you? ";
+    }
+
+    @RequestMapping("/hi")
+    public String getHi(@RequestParam(value="fname") String fname, @RequestParam(value="age") String age){
+        return "Hello. How are you? Your name is "+fname+" and you are "+age;
+    }
+
+    @RequestMapping(method= RequestMethod.POST, value="/login")
+    public String login(@RequestBody String credential){
+        System.out.println(credential);
+        return "{\"Error\":\"Login already exists\"}";
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value="/signup")
+    public String signup(@RequestBody String data){
+        System.out.println(data);
+
+        return data;
+    }
+
+}
